c40244f418d610125116c4f07f28bc2f9f2a5426
pavan <pavan@LAPTOP-B4SBV2JI>
2019-12-11 09:37:47 +0530

    Map Program

--- src/main/java/week3/day2/MapClassRoom.java
+++ src/main/java/week3/day2/MapClassRoom.java
@@ -7,19 +7,26 @@ public class MapClassRoom {
 	
 	public static void main(String[] args)
 	{
+		// Create a String with your name as value
 		String text="PAVAN";
+		// Convert the string into array
 		char[] arry = text.toCharArray();
-		
+		// Create an empty map<Character,Integer>
 		Map<Character, Integer> map = new HashMap<>();
-		
-		for (char c : arry) {
-			if (map.containsKey(Character) ) {
-				System.out.println(map.get(c));
+		// Iterate over the array
+		for (char eachChar : arry) {
+			//if(map.contains(Character))
+			if (map.containsKey(eachChar)) {
+				// get the value using key +1
+				Integer value = map.get(eachChar)+1;
+				map.put(eachChar, value);
 				
 			} else {
-
+				// add the character in the map & set the value as 1
+				map.put(eachChar, 1);
 			}
 		}
+		System.out.println(map);
 	}
 	
 
